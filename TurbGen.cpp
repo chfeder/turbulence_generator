2d68f7cff39ff24f057a1b8615903361648e3e13
Christoph Federrath <chfeder@TwisterMini.local>
2022-06-26 23:19:07 +1000

    support for 1.5D and 2.5D turbulent field generation

--- TurbGen.cpp
+++ TurbGen.cpp
@@ -7,8 +7,9 @@
 #include <iostream>
 #include <iomanip>
 #include <sstream>
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
 #include "TurbGen.h"
 
 // normally set via compiler defines: #define HAVE_HDF5
@@ -33,7 +34,7 @@ using namespace std;
 enum {X, Y, Z};
 static const string ProgSign = "TurbGen.cpp: ";
 int verbose = 1; // 0: all standard output off (quiet mode)
-int ndim = 3; // dimensionality (must be 1 or 2 or 3)
+double ndim = 3; // dimensionality (must be 1 or 1.5 or 2 or 2.5 or 3)
 int N[3] = {64, 64, 64}; // number of cells in turbulent output field in x, y, z
 double L[3] = {1.0, 1.0, 1.0}; // size of box in x, y, z
 double k_min = 2.0; // minimum wavenumber for turbulent field (in units of 2pi / L[X])
@@ -47,6 +48,7 @@ double angles_exp = 1.0; // if spect_form == 2: spectral sampling of angles;
 double sol_weight = 0.5; // solenoidal weight: 1.0: solenoidal driving, 0.0: compressive driving, 0.5: natural mixture
 int random_seed = 140281; // random seed for this turbulent realisation
 string outfilename = "TurbGen_output.h5"; // HDF5 output filename
+bool write_modes = false; // switch to write Fourier modes and amplitudes to output file
 
 // MPI stuff
 int MyPE = 0, NPE = 1;
@@ -99,9 +101,12 @@ int main(int argc, char * argv[])
     // initialise generator to return a single turbulent realisation based on input parameters
     tg.init_single_realisation(ndim, L, k_min, k_max, spect_form, power_law_exp, angles_exp, sol_weight, random_seed);
 
+    // get the number of vector field components
+    int ncmp = tg.get_number_of_components();
+
     // set dimensionality dependencies
-    if (ndim < 3) { N[Z] = 1; L[Z] = 1.0; } // 2D
-    if (ndim < 2) { N[Y] = 1; L[Y] = 1.0; } // 1D
+    if ((int)ndim < 3) { N[Z] = 1; L[Z] = 1.0; } // 2D
+    if ((int)ndim < 2) { N[Y] = 1; L[Y] = 1.0; } // 1D
 
     // define cell size of uniform grid
     double del[3];
@@ -153,16 +158,16 @@ int main(int argc, char * argv[])
     // output grid, which receives the turbulent field (up to ndim = 3)
     float * grid_out[3];
     // allocate
-    for (int d = 0; d < ndim; d++) grid_out[d] = new float[ntot];
+    for (int d = 0; d < ncmp; d++) grid_out[d] = new float[ntot];
 
-    // call to return uniform grid(s) with ndim components of the turbulent field at requested positions
+    // call to return uniform grid(s) with ncmp components of the turbulent field at requested positions
     tg.get_turb_vector_unigrid(pos_beg, pos_end, N_out, grid_out);
 
     // compute mean and std of generated turbulent field and then re-normalise to mean=0 and std=1
     double mean [3] = {0.0, 0.0, 0.0};
     double mean2[3] = {0.0, 0.0, 0.0};
     double std[3] = {0.0, 0.0, 0.0};
-    for (int d = 0; d < ndim; d++) {
+    for (int d = 0; d < ncmp; d++) {
         for (long ni = 0; ni < ntot; ni++) {
             mean [d] += grid_out[d][ni];
             mean2[d] += pow(grid_out[d][ni],2.0);
@@ -172,7 +177,7 @@ int main(int argc, char * argv[])
     MPI_Allreduce(MPI_IN_PLACE, mean , 3, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     MPI_Allreduce(MPI_IN_PLACE, mean2, 3, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 #endif
-    for (int d = 0; d < ndim; d++) {
+    for (int d = 0; d < ncmp; d++) {
         mean [d] /= N[X]*N[Y]*N[Z]; // mean
         mean2[d] /= N[X]*N[Y]*N[Z]; // mean squared
         std[d] = sqrt(mean2[d] - mean[d]*mean[d]); // standard deviation
@@ -183,7 +188,7 @@ int main(int argc, char * argv[])
     }
 
     // re-compute mean and std
-    for (int d = 0; d < ndim; d++) {
+    for (int d = 0; d < ncmp; d++) {
         mean[d] = 0.0; mean2[d] = 0.0;
         for (long ni = 0; ni < ntot; ni++) {
             mean [d] += grid_out[d][ni];
@@ -194,7 +199,7 @@ int main(int argc, char * argv[])
     MPI_Allreduce(MPI_IN_PLACE, mean , 3, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     MPI_Allreduce(MPI_IN_PLACE, mean2, 3, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 #endif
-    for (int d = 0; d < ndim; d++) {
+    for (int d = 0; d < ncmp; d++) {
         mean [d] /= N[X]*N[Y]*N[Z]; // mean
         mean2[d] /= N[X]*N[Y]*N[Z]; // mean squared
         std[d] = sqrt(mean2[d] - mean[d]*mean[d]); // standard deviation
@@ -202,14 +207,14 @@ int main(int argc, char * argv[])
 
     if (MyPE==0 && verbose>0) {
         cout<<ProgSign+"Generated "<<ndim<<"D turbulent field with"<<endl;
-        cout<<ProgSign+" number of grid cells N ="; for (int d = 0; d < ndim; d++) cout<<" "<<N[d]; cout<<endl;
-        cout<<ProgSign+" physical size L ="; for (int d = 0; d < ndim; d++) cout<<" "<<L[d]; cout<<endl;
-        cout<<ProgSign+" mean ="; for (int d = 0; d < ndim; d++) cout<<" "<<mean[d]; cout<<endl;
-        cout<<ProgSign+" 1D standard deviation ="; for (int d = 0; d < ndim; d++) cout<<" "<<std[d]; cout<<endl;
+        cout<<ProgSign+" number of grid cells N ="; for (int d = 0; d < (int)ndim; d++) cout<<" "<<N[d]; cout<<endl;
+        cout<<ProgSign+" physical size L ="; for (int d = 0; d < (int)ndim; d++) cout<<" "<<L[d]; cout<<endl;
+        cout<<ProgSign+" mean (for each component) ="; for (int d = 0; d < ncmp; d++) cout<<" "<<mean[d]; cout<<endl;
+        cout<<ProgSign+" 1D standard deviation (for each component) ="; for (int d = 0; d < ncmp; d++) cout<<" "<<std[d]; cout<<endl;
         string expected = "";
-        if (ndim==1) expected = "1";
-        if (ndim==2) expected = "sqrt(2)";
-        if (ndim==3) expected = "sqrt(3)";
+        if (ncmp==1) expected = "1";
+        if (ncmp==2) expected = "sqrt(2)";
+        if (ncmp==3) expected = "sqrt(3)";
         cout<<ProgSign+" total ("<<ndim<<"D) standard deviation (expected: "<<expected<<") = "<<sqrt(std[0]*std[0]+std[1]*std[1]+std[2]*std[2])<<endl;
     }
 
@@ -223,7 +228,8 @@ int main(int argc, char * argv[])
     hdfio.create(outfilename, MPI_COMM);
     // write scalars
     vector<int> hdf5dims(0);
-    hdfio.write(&ndim, "ndim", hdf5dims, H5T_NATIVE_INT, MPI_COMM);
+    hdfio.write(&ndim, "ndim", hdf5dims, H5T_NATIVE_DOUBLE, MPI_COMM);
+    hdfio.write(&ncmp, "ncmp", hdf5dims, H5T_NATIVE_INT, MPI_COMM);
     hdfio.write(&k_min, "kmin", hdf5dims, H5T_NATIVE_DOUBLE, MPI_COMM);
     hdfio.write(&k_max, "kmax", hdf5dims, H5T_NATIVE_DOUBLE, MPI_COMM);
     hdfio.write(&spect_form, "spect_form", hdf5dims, H5T_NATIVE_INT, MPI_COMM);
@@ -234,27 +240,27 @@ int main(int argc, char * argv[])
     hdfio.write(&sol_weight, "sol_weight", hdf5dims, H5T_NATIVE_DOUBLE, MPI_COMM);
     hdfio.write(&random_seed, "random_seed", hdf5dims, H5T_NATIVE_INT, MPI_COMM);
     // write N and L vectors
-    hdf5dims.resize(1); hdf5dims[0] = ndim;
-    int No[ndim]; double Lo[ndim]; // order Z,Y,X
-     for (int d = 0; d < ndim; d++) {
-        No[ndim-1-d] = N[d];
-        Lo[ndim-1-d] = L[d];
+    hdf5dims.resize(1); hdf5dims[0] = (int)ndim;
+    int No[(int)ndim]; double Lo[(int)ndim]; // order Z,Y,X
+     for (int d = 0; d < (int)ndim; d++) {
+        No[(int)ndim-1-d] = N[d];
+        Lo[(int)ndim-1-d] = L[d];
     }
     hdfio.write(No, "N", hdf5dims, H5T_NATIVE_INT, MPI_COMM);
     hdfio.write(Lo, "L", hdf5dims, H5T_NATIVE_DOUBLE, MPI_COMM);
     // write turbulent field (components)
-    hdf5dims.resize(ndim); for (int d = 0; d < ndim; d++) hdf5dims[ndim-1-d] = N[d]; // order Z,Y,X
-    if (MyPE==0 && verbose>1) { cout<<ProgSign+"hdf5dims ="; for (int d = 0; d < ndim; d++) cout<<" "<<hdf5dims[d]; cout<<endl; }
-    for (int dc = 0; dc < ndim; dc++) { // loop over component(s)
+    hdf5dims.resize((int)ndim); for (int d = 0; d < (int)ndim; d++) hdf5dims[(int)ndim-1-d] = N[d]; // order Z,Y,X
+    if (MyPE==0 && verbose>1) { cout<<ProgSign+"hdf5dims ="; for (int d = 0; d < (int)ndim; d++) cout<<" "<<hdf5dims[d]; cout<<endl; }
+    for (int dc = 0; dc < ncmp; dc++) { // loop over component(s)
         string dsetname = "turb_field";
         if (dc == 0) dsetname += "_x";
         if (dc == 1) dsetname += "_y";
         if (dc == 2) dsetname += "_z";
         hdfio.create_dataset(dsetname, hdf5dims, H5T_NATIVE_FLOAT, MPI_COMM); // create HDF5 dataset
         // specify dimensions and offset for slab operation
-        hsize_t offset[ndim], count[ndim], out_offset[ndim], out_count[ndim];
-        for (int d = 0; d < ndim; d++) {
-            int dd = ndim-1-d; // order Z,Y,X
+        hsize_t offset[(int)ndim], count[(int)ndim], out_offset[(int)ndim], out_count[(int)ndim];
+        for (int d = 0; d < (int)ndim; d++) {
+            int dd = (int)ndim-1-d; // order Z,Y,X
             // collective HDF5 IO only works if inactive cores participate, but with counts=0 and offsets=0
             offset[dd] = 0; count[dd] = 0; out_offset[dd] = 0; out_count[dd] = 0;
             if (MyPE < NPE_in_use) {
@@ -265,20 +271,40 @@ int main(int argc, char * argv[])
             }
         }
         if (verbose>1) {
-            for (int d = 0; d < ndim; d++)
+            for (int d = 0; d < (int)ndim; d++)
                 cout<<"MyPE, d, offset, count, out_offset, out_count = "<<
                         MyPE<<" "<<d<<" "<<offset[d]<<" "<<count[d]<<" "<<out_offset[d]<<" "<<out_count[d]<<endl;
         }
         // write slab to file (in parallel, if we have MPI)
-        hdfio.overwrite_slab(grid_out[dc], dsetname, H5T_NATIVE_FLOAT, offset, count, ndim, out_offset, out_count, MPI_COMM);
+        hdfio.overwrite_slab(grid_out[dc], dsetname, H5T_NATIVE_FLOAT, offset, count, (int)ndim, out_offset, out_count, MPI_COMM);
         if (MyPE==0 && verbose>0) cout<<ProgSign+"Dataset '"<<dsetname<<"' in '"<<outfilename<<"' written."<<endl;
+    }
+    // output generating modes and their amplitudes
+    if (write_modes) {
+        // modes
+        vector< vector<double> > modes = tg.get_modes();
+        int nmodes = modes[0].size(); // all dims have the same number of modes
+        hdf5dims.resize(2); hdf5dims[0] = (int)ndim; hdf5dims[1] = nmodes;
+        double * ptmp = new double[((int)ndim)*nmodes];
+        for (int d = 0; d < (int)ndim; d++)
+            for (int m = 0; m < nmodes; m++)
+                ptmp[d*nmodes+m] = modes[d][m];
+        hdfio.write(ptmp, "Fourier_modes", hdf5dims, H5T_NATIVE_DOUBLE, MPI_COMM);
+        delete [] ptmp;
+        // amplitudes
+        vector<double> amplitudes = tg.get_amplitudes();
+        ptmp = new double[amplitudes.size()];
+        for (int i = 0; i < amplitudes.size(); i++) ptmp[i] = amplitudes[i];
+        hdf5dims.resize(1); hdf5dims[0] = amplitudes.size();
+        hdfio.write(ptmp, "Fourier_amplitudes", hdf5dims, H5T_NATIVE_DOUBLE, MPI_COMM);
+        delete [] ptmp;
     }
     hdfio.close();
     if (MyPE==0 && verbose>0) cout<<ProgSign+"Finished writing '"<<outfilename<<"'."<<endl;
 #endif
 
     // clean up
-    for (int d = 0; d < ndim; d++) {
+    for (int d = 0; d < ncmp; d++) {
       if (grid_out[d]) delete [] grid_out[d]; grid_out[d] = NULL;
     }
 
@@ -336,18 +362,18 @@ int ParseInputs(const vector<string> Argument)
         }
         if (Argument[i] != "" && Argument[i] == "-N")
         {
-            if (Argument.size()>i+ndim) {
+            if (Argument.size()>i+(int)ndim) {
                 dummystream << Argument[i+1]; dummystream >> N[X]; dummystream.clear();
-                if (ndim > 1) { dummystream << Argument[i+2]; dummystream >> N[Y]; dummystream.clear(); }
-                if (ndim > 2) { dummystream << Argument[i+3]; dummystream >> N[Z]; dummystream.clear(); }
+                if ((int)ndim > 1) { dummystream << Argument[i+2]; dummystream >> N[Y]; dummystream.clear(); }
+                if ((int)ndim > 2) { dummystream << Argument[i+3]; dummystream >> N[Z]; dummystream.clear(); }
             } else return -1;
         }
         if (Argument[i] != "" && Argument[i] == "-L")
         {
-            if (Argument.size()>i+ndim) {
+            if (Argument.size()>i+(int)ndim) {
                 dummystream << Argument[i+1]; dummystream >> L[X]; dummystream.clear();
-                if (ndim > 1) { dummystream << Argument[i+2]; dummystream >> L[Y]; dummystream.clear(); }
-                if (ndim > 2) { dummystream << Argument[i+3]; dummystream >> L[Z]; dummystream.clear(); }
+                if ((int)ndim > 1) { dummystream << Argument[i+2]; dummystream >> L[Y]; dummystream.clear(); }
+                if ((int)ndim > 2) { dummystream << Argument[i+3]; dummystream >> L[Z]; dummystream.clear(); }
             } else return -1;
         }
         if (Argument[i] != "" && Argument[i] == "-kmin")
@@ -398,6 +424,10 @@ int ParseInputs(const vector<string> Argument)
                 dummystream << Argument[i+1]; dummystream >> outfilename; dummystream.clear();
             } else return -1;
         }
+        if (Argument[i] != "" && Argument[i] == "-write_modes")
+        {
+            write_modes = true;
+        }
     } // loop over all args
 
     /// print out parsed values
@@ -422,19 +452,20 @@ void HelpMe(void)
         << "Syntax:" << endl
         << " TurbGen [<OPTIONS>]" << endl << endl
         << "   <OPTIONS>:" << endl
-        << "     -ndim <1, 2, 3>        : number of spatial dimensions; (default: 3)" << endl
-        << "     -N <Nx [Ny [Nz]]>      : number of grid cells for generated turbulent field; (default: 64 64 64)" << endl
-        << "     -L <Lx [Ly [Lz]]>      : physical size of box in which to generate turbulent field; (default: 1.0 1.0 1.0)" << endl
-        << "     -kmin <val>            : minimum wavenumber of generated field in units of 2pi/L[X]; (default: 2.0)" << endl
-        << "     -kmax <val>            : maximum wavenumber of generated field in units of 2pi/L[X]; (default: 20.0)" << endl
-        << "     -spect_form <0, 1, 2>  : spectral form: 0 (band/rectangle/constant), 1 (paraboloid), 2 (power law); (default: 2)" << endl
-        << "     -power_law_exp <val>   : if spect_form 2: power-law exponent of power-law spectrum (e.g. Kolmogorov: -5/3, Burgers: -2, Kazantsev: 1.5); (default -2.0)" << endl
-        << "     -angles_exp <val>      : if spect_form 2: angles exponent for sparse sampling (e.g., 2.0: full sampling, 0.0: healpix-like sampling); (default 1.0)" << endl
-        << "     -sol_weight <val>      : solenoidal weight: 1.0 (divergence-free field), 0.5 (natural mix), 0.0 (curl-free field); (default 0.5)" << endl
-        << "     -random_seed <val>     : random seed for turbulent field; (default 140281)" << endl
-        << "     -verbose <0, 1, 2>     : 0 (no shell output), 1 (standard shell output), 2 (more shell output); (default 1)" << endl
-        << "     -o <filename>          : output filename (for HDF5 output); (default 'TurbGen_output.h5')" << endl
-        << "     -h                     : print this help message" << endl
+        << "     -ndim <1, 1.5, 2, 2.5, 3> : number of spatial dimensions; (default: 3)" << endl
+        << "     -N <Nx [Ny [Nz]]>         : number of grid cells for generated turbulent field; (default: 64 64 64)" << endl
+        << "     -L <Lx [Ly [Lz]]>         : physical size of box in which to generate turbulent field; (default: 1.0 1.0 1.0)" << endl
+        << "     -kmin <val>               : minimum wavenumber of generated field in units of 2pi/L[X]; (default: 2.0)" << endl
+        << "     -kmax <val>               : maximum wavenumber of generated field in units of 2pi/L[X]; (default: 20.0)" << endl
+        << "     -spect_form <0, 1, 2>     : spectral form: 0 (band/rectangle/constant), 1 (paraboloid), 2 (power law); (default: 2)" << endl
+        << "     -power_law_exp <val>      : if spect_form 2: power-law exponent of power-law spectrum (e.g. Kolmogorov: -5/3, Burgers: -2, Kazantsev: 1.5); (default: -2.0)" << endl
+        << "     -angles_exp <val>         : if spect_form 2: angles exponent for sparse sampling (e.g., 2.0: full sampling, 0.0: healpix-like sampling); (default: 1.0)" << endl
+        << "     -sol_weight <val>         : solenoidal weight: 1.0 (divergence-free field), 0.5 (natural mix), 0.0 (curl-free field); (default: 0.5)" << endl
+        << "     -random_seed <val>        : random seed for turbulent field; (default: 140281)" << endl
+        << "     -verbose <0, 1, 2>        : 0 (no shell output), 1 (standard shell output), 2 (more shell output); (default: 1)" << endl
+        << "     -o <filename>             : output filename (for HDF5 output); (default: TurbGen_output.h5)" << endl
+        << "     -write_modes              : write generating Fourier modes and amplitudes to output file" << endl
+        << "     -h                        : print this help message" << endl
         << endl
         << "Example: TurbGen -ndim 2 -L 1.0 1.0"
         << endl << endl;
