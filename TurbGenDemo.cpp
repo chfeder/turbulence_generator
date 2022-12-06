932f60e40e3d4c9a9ffb3c7da843f9ba5a34f652
Christoph Federrath <chfeder@TwisterMini.local>
2022-12-07 10:41:54 +1100

    new version of TurbGen

--- TurbGenDemo.cpp
+++ TurbGenDemo.cpp
@@ -3,46 +3,51 @@
  *** Written by Christoph Federrath, 2022.              ***
 ***********************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <string>
 #include "TurbGen.h"
 
 int main(void)
 {
+  std::string funcsig = "TurbGenDemo: ";
+  std::string parameter_file = "TurbGen.par";
+
   // 1st example case (turbulence driving)
   if (true) {
-    printf("turbulence_generator: ==========================================================\n");
+    printf("\n%s === 1st example case (turbulence driving) ===\n", funcsig.c_str());
 
     // create TurbGen class object
     TurbGen tg = TurbGen();
 
-    // First, initialise the turbulence generator with input parameters from 'parameter_file'.
-    // If MPI is present, we pass the MPI rank to TurbGen_init_turbulence_generator;
+    // First, initialise the turbulence generator with input parameters from parameter_file
+    // If MPI is present, we pass the MPI rank to TurbGen.init_driving(...);
     // note that the MPI rank is simply to control printf to stdout, i.e., only rank 0 will print.
     // This function should only be called once, i.e., to initialise the turbulence generator.
-    tg.init_driving("turbulence_generator.inp");
+    tg.init_driving(parameter_file);
 
     // as an example, let's loop through a few time steps (e.g., of a hydro code)
     // to demonstrate how the turbulence generator is updated and how the physical turbulent
-    // acceleration field is returned by "turbulence_generator.h
+    // acceleration field is returned by TurbGen.h
 
     // get the turbulent turnover time
     double t_turb = tg.get_turnover_time();
-    // let's go through 5 turnover times based on the inputs in 'parameter_file' above
+    // let's go through 5 turnover times based on the inputs in parameter_file above
     double t_end = 5.0 * t_turb;
 
     // set some number of time steps that the hydro code would go through
     int nsteps = 123;
     // time step (this will normally be set by the hydro code;
     // ultimately, one simply feeds in the 'time' at which one wants the respective
-    // physical turbulent acceleration field -- see below in call to TurbGen_check_for_update
+    // physical turbulent acceleration field -- see below in call to TurbGen.check_for_update(...)
     double dt = t_end / nsteps;
 
     // loop through time steps
     for (int step = 0; step <= nsteps; step++) {
 
       double time = step * dt;
-      printf("turbulence_generator: time = %f\n", time);
+      printf("%stime = %f\n", funcsig.c_str(), time);
 
       // Function to update the turbulence driving mode coefficients.
       // Based on input 'time', it checks if the pattern needs to be updated.
@@ -57,8 +62,8 @@ int main(void)
         double pos[3] = {0.1, -0.5, 0.3};
         double v[3]; // return vector
         tg.get_turb_vector(pos, v); // get the vector
-        printf("turbulence_generator: turbulent vector at position (%f %f %f) is vx, vy, vz = %f %f %f\n",
-                pos[0], pos[1], pos[2], v[0], v[1], v[2]);
+        printf("%sturbulent vector at position (%f %f %f) is vx, vy, vz = %f %f %f\n",
+               funcsig.c_str(), pos[0], pos[1], pos[2], v[0], v[1], v[2]);
       }
 
     } // end of loop over timesteps
@@ -68,7 +73,8 @@ int main(void)
 
   // 2nd example case (return turbulent vector field on uniform grid)
   if (true) {
-    printf("turbulence_generator: ==========================================================\n");
+    printf("\n%s === 2nd example case (fast forward to 5 t_turb and return turbulent vector field on uniform grid) ===\n",
+           funcsig.c_str());
 
     // create TurbGen class object
     TurbGen tg = TurbGen();
@@ -79,7 +85,7 @@ int main(void)
     // at every requested position; here, it is done for an entire block (uniform grid) of data.
 
     // first init as in example 1
-    tg.init_driving("turbulence_generator.inp");
+    tg.init_driving(parameter_file);
 
     // now fast forward to target time (e.g., after 5 turbulent turnover times)
     tg.check_for_update(5*tg.get_turnover_time());
@@ -89,7 +95,7 @@ int main(void)
     double pos_end[3] = {0.4, -0.3, 0.5}; // last  cell coordinate in uniform grid (x,y,z)
     int n[3] = {10, 20, 13}; // some random number of cells that define the output grid size
     long ntot = n[0]*n[1]*n[2]; // total number of grid cells
-    float * grid_out[3]; // output grid (three cartesian components: vx, vy, vz), which receives the turbulent acceleration field
+    float * grid_out[3]; // output grid (3 cartesian components: vx, vy, vz), receiving the turbulent acceleration field
 
     // allocate
     for (int dim = 0; dim < 3; dim++) grid_out[dim] = new float[ntot];
@@ -101,10 +107,12 @@ int main(void)
     int i, j, k; long index;
     i = 3; j = 0; k = 10;
     index = k*n[1]*n[0] + j*n[0] + i;
-    printf("turbulence_generator: e.g., x-component of turbulent vector field at index i,j,k = (%i %i %i) is %f\n", i,j,k, grid_out[0][index]);
+    printf("%se.g., x-component of turbulent vector field at index i,j,k = (%i %i %i) is %f\n",
+           funcsig.c_str(), i, j, k, grid_out[0][index]);
     i = 9; j = 1; k = 4;
     index = k*n[1]*n[0] + j*n[0] + i;
-    printf("turbulence_generator: e.g., z-component of turbulent vector field at index i,j,k = (%i %i %i) is %f\n", i,j,k, grid_out[2][index]);
+    printf("%se.g., z-component of turbulent vector field at index i,j,k = (%i %i %i) is %f\n",
+           funcsig.c_str(), i, j, k, grid_out[2][index]);
 
     // clean up
     for (int dim = 0; dim < 3; dim++) {
@@ -116,7 +124,7 @@ int main(void)
 
   // 3rd example case (single realisation)
   if (true) {
-    printf("turbulence_generator: ==========================================================\n");
+    printf("\n%s === 3rd example case (single realisation) ===\n", funcsig.c_str());
 
     // create TurbGen class object
     TurbGen tg = TurbGen();
@@ -141,8 +149,8 @@ int main(void)
     // random example of physical coordinates of a target output grid (pos_beg must be < pos_end):
     int n[3] = {64, 64, 64}; // some random number of cells that define the output grid size
     double d[3]; for (int dim = 0; dim < 3; dim++) d[dim] = L[dim] / n[dim]; // define cell size of uniform grid
-    double pos_beg[3] = {-0.5+d[0]/2, -0.5+d[1]/2, -0.5+d[2]/2}; // first cell coordinate in uniform grid (x,y,z); here cell-centered example
-    double pos_end[3] = {+0.5-d[0]/2, +0.5-d[1]/2, +0.5-d[2]/2}; // last  cell coordinate in uniform grid (x,y,z); here cell-centered example
+    double pos_beg[3] = {-0.5+d[0]/2, -0.5+d[1]/2, -0.5+d[2]/2}; // first cell coordinate in uniform grid (x,y,z); cell-centered example
+    double pos_end[3] = {+0.5-d[0]/2, +0.5-d[1]/2, +0.5-d[2]/2}; // last  cell coordinate in uniform grid (x,y,z); cell-centered example
     long ntot = n[0]*n[1]*n[2]; // total number of grid cells
     float * grid_out[3]; // output grid (three cartesian components: vx, vy, vz), which receives the turbulent acceleration field
 
@@ -159,10 +167,12 @@ int main(void)
     int i, j, k; long index;
     i = 3; j = 0; k = 10;
     index = k*n[1]*n[0] + j*n[0] + i;
-    printf("turbulence_generator: e.g., x-component of turbulent vector field at index i,j,k = (%i %i %i) is %f\n", i,j,k, grid_out[0][index]);
+    printf("%se.g., x-component of turbulent vector field at index i,j,k = (%i %i %i) is %f\n",
+           funcsig.c_str(), i, j, k, grid_out[0][index]);
     i = 9; j = 1; k = 4;
     index = k*n[1]*n[0] + j*n[0] + i;
-    printf("turbulence_generator: e.g., z-component of turbulent vector field at index i,j,k = (%i %i %i) is %f\n", i,j,k, grid_out[2][index]);
+    printf("%se.g., z-component of turbulent vector field at index i,j,k = (%i %i %i) is %f\n",
+           funcsig.c_str(), i, j, k, grid_out[2][index]);
 
     // compute mean and RMS of vector field
     double mean[3] = {0.0, 0.0, 0.0};
@@ -177,10 +187,12 @@ int main(void)
       rms[dim] = sqrt(rms[dim] / ntot); // root mean squared
       std[dim] = sqrt(rms[dim]*rms[dim] - mean[dim]*mean[dim]); // standard deviation
     }
-    printf("turbulence_generator: Turbulent vector field mean (x,y,z) = (%e %e %e)\n", mean[0], mean[1], mean[2]);
-    printf("turbulence_generator: Turbulent vector field  rms (x,y,z) = (%e %e %e)\n",  rms[0],  rms[1],  rms[2]);
-    printf("turbulence_generator: Turbulent vector field  std (x,y,z) = (%e %e %e)\n",  std[0],  std[1],  std[2]);
-    printf("turbulence_generator: Turbulent vector field total 3D std = %e\n", sqrt(std[0]*std[0]+std[1]*std[1]+std[2]*std[2]));
+    printf("%sTurbulent vector field mean (x,y,z) = (%e %e %e)\n", funcsig.c_str(), mean[0], mean[1], mean[2]);
+    printf("%sTurbulent vector field  rms (x,y,z) = (%e %e %e)\n", funcsig.c_str(),  rms[0],  rms[1],  rms[2]);
+    printf("%sTurbulent vector field  std (x,y,z) = (%e %e %e)\n", funcsig.c_str(),  std[0],  std[1],  std[2]);
+    printf("%sTurbulent vector field total 3D std = %e\n", funcsig.c_str(), sqrt(std[0]*std[0]+std[1]*std[1]+std[2]*std[2]));
+    printf("%sNote that the standard deviations will usually need to be normalised to the target std for single realisations.\n%s",
+           funcsig.c_str(), "             See TurbGen for a full program that does this.\n");
 
     // clean up
     for (int dim = 0; dim < 3; dim++) {
